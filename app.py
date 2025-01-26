7065481247d65ec7f45c9797488034110a124127
abderbj <abderrahman.bejaoui@insat.ucar.tn>
2025-01-26 08:30:40 +0100

    he

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 import subprocess
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes and origins
 from pipline1 import top_three
 def ask_ollama(prompt):
     # Refined prompt to enhance the chatbot's responses
